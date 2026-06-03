f7375d7bd5b2f06c4be8e600e99e8df91439d30b
MeenalGoyalTesting <meenal.goyal@testingxperts.com>
2026-06-03 18:27:51 +0530

    Initial Commit

---
+++ Security/src/main/java/com/securityBasics/Security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.securityBasics.Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
