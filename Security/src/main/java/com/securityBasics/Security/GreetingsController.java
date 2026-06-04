fa91dfc44e2c0343c55bb5661f2f7bc95bd77217
MeenalGoyalTesting <meenal.goyal@testingxperts.com>
2026-06-04 18:16:26 +0530

    Role based

--- Security/src/main/java/com/securityBasics/Security/GreetingsController.java
+++ Security/src/main/java/com/securityBasics/Security/GreetingsController.java
@@ -1,6 +1,8 @@
 package com.securityBasics.Security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,16 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "hello user";
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "hello admin";
     }
 }
